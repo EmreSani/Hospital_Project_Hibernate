89cdc5f33f9175b29fe98c535b84a1b42b34c7a7
EmreSani <emresani1998@gmail.com>
2024-05-06 16:46:23 +0300

    todo added

--- src/main/java/com/hospitalproject/controller/HospitalApplicationRunner.java
+++ src/main/java/com/hospitalproject/controller/HospitalApplicationRunner.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class HospitalApplicationRunner {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
+//TODO: methods to fix: doctor update, doctor remove, patient update, patient remove and related methods.
         HospitalManagementSystem.start();
 
     }
